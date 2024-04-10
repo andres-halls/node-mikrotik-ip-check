6294153c925fc112b5540b16dcc171a1d8cb021a
andres-halls <8243886+andres-halls@users.noreply.github.com>
2024-04-10 03:01:13 +0300

    fix: filter out domain names in address list

--- index.js
+++ index.js
@@ -16,27 +16,45 @@ if (!LISTNAME) throw new Error('MIKROTIK_ADDRESS_LIST missing from env!');
 const server = http.createServer(async (req, res) => {
   try {
     const requestUrl = req.headers['host'] + req.headers['x-original-uri'];
-    let requestIp = req.headers['x-real-ip'] || req.connection.remoteAddress;
-    requestIp = ip6addr.parse(requestIp);
+    let requestIP = req.headers['x-real-ip'] || req.connection.remoteAddress;
+    requestIP = ip6addr.parse(requestIP);
     const options = {PORT, closeOnDone: true, timeout: 5, closeOnTimeout: true};
     const connection = MikroNode.getConnection(HOST, USERNAME, PASSWORD, options);
     const conn = await connection.getConnectPromise();
     let ipv4List = conn.getCommandPromise('/ip/firewall/address-list/print');
     let ipv6List = conn.getCommandPromise('/ipv6/firewall/address-list/print');
     [ipv4List, ipv6List] = await Promise.all([ipv4List, ipv6List]);
+
     ipv4List = ipv4List
       .filter(ip => ip.list === LISTNAME && ip.disabled === 'false')
-      .map(ip => ip6addr.parse(ip.address))
+      .filter(ip => {
+        try {
+          ip6addr.parse(ip.address);
+          return true;
+        } catch (err) {
+          return false;
+        }
+      })
+      .map(ip => ip6addr.parse(ip.address));
+
     ipv6List = ipv6List
       .filter(ip => ip.list === LISTNAME && ip.disabled === 'false')
+      .filter(ip => {
+        try {
+          ip6addr.createCIDR(ip.address);
+          return true;
+        } catch (err) {
+          return false;
+        }
+      })
       .map(ip => ip6addr.createCIDR(ip.address));
 
     let allowed = false;
 
-    if (requestIp.kind() === 'ipv4') {
-      allowed = ipv4List.some(ip => ip.compare(requestIp) === 0);
-    } else if (requestIp.kind() === 'ipv6') {
-      allowed = ipv6List.some(cidr => cidr.contains(requestIp));
+    if (requestIP.kind() === 'ipv4') {
+      allowed = ipv4List.some(ip => ip.compare(requestIP) === 0);
+    } else if (requestIP.kind() === 'ipv6') {
+      allowed = ipv6List.some(cidr => cidr.contains(requestIP));
     } else {
       throw new Error('Request IP has unknown kind.');
     }
@@ -46,12 +64,12 @@ const server = http.createServer(async (req, res) => {
       return res.end();
     }
 
-    console.log(chalk`{red [!]} IP {bold ${requestIp}} not allowed.`);
+    console.log(chalk`{red [!]} IP {bold ${requestIP}} not allowed.`);
     console.log(`\tRequested URL: ${requestUrl}`);
     res.statusCode = 403;
     res.end();
   } catch (err) {
-    console.error(err.toString());
+    console.error(err.stack);
     res.statusCode = 500;
     res.end();
   }
